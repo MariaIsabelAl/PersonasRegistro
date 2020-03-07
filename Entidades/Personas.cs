662fc035e4d9774a79f9307a816ecd0f62626701
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-03-06 20:41:17 -0400

    Add project files.

---
+++ Entidades/Personas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Personas.Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
+
+        public Personas()
+        {
+            PersonaId = 0;
+            Nombre = string.Empty;
+            
+        }
+    }
+}
