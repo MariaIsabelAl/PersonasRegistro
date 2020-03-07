5c126d1b9d93d9a0a8e1ade3960e14ad5c2e1d2a
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-03-06 20:49:52 -0400

    Hice la migracion Persona y cree la tabla Personas.db

---
+++ Migrations/20200307004836_Persona.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Personas.Migrations
+{
+    public partial class Persona : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    PersonaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.PersonaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
